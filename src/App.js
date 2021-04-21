f445b999c0bcf2d773076e725710936c5d20eeb5
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-04-21 05:29:46 +0300

    Move data to state

--- src/App.js
+++ src/App.js
@@ -11,54 +11,53 @@ class App extends React.Component {
     // Moblie first
     this.state = {
       isOpen: false,
-      isMobile: true
+      isMobile: true,
+      previousWidth: -1,
     };
-
-    this.previousWidth = -1;
   }
 
   updateWidth() {
     const width = window.innerWidth;
     const widthLimit = 576;
     const isMobile = width <= widthLimit;
-    const wasMobile = this.previousWidth <= widthLimit;
+    const wasMobile = this.state.previousWidth <= widthLimit;
 
     if (isMobile !== wasMobile) {
       this.setState({
-        isOpen: !isMobile
+        isMobile: !isMobile,
+        isOpen: !isMobile,
+        previousWidth: width,
       });
     }
-
-    this.previousWidth = width;
   }
 
   /**
    * Add event listener
    */
-  componentDidMount() {
-    this.updateWidth();
-    window.addEventListener("resize", this.updateWidth.bind(this));
-  }
+    componentDidMount() {
+        this.updateWidth();
+        window.addEventListener("resize", this.updateWidth.bind(this));
+    }
 
   /**
    * Remove event listener
    */
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWidth.bind(this));
-  }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateWidth.bind(this));
+    }
 
-  toggle = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
+    toggle = () => {
+        this.setState({ ...this.state, isOpen: !this.state.isOpen });
+    };
 
-  render() {
-    return (
-      <div className="App wrapper">
-        <SideBar toggle={this.toggle} isOpen={this.state.isOpen} />
-        <Content toggle={this.toggle} isOpen={this.state.isOpen} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App wrapper">
+                <SideBar toggle={this.toggle} isOpen={this.state.isOpen} />
+                <Content toggle={this.toggle} isOpen={this.state.isOpen} />
+            </div>
+        );
+    }
 }
 
 export default App;
